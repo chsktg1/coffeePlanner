4305d2fea6f98c87b9f1134b52d545ea7804309f
chsk <sumanthkumar031@gmail.com>
2021-07-21 08:39:08 +0000

    max-depth-exceeded

---
+++ src/components/QuestionOption/index.js
@@ -0,0 +1,48 @@
+// Write your code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class QuestionOption extends Component {
+  //   constructor(props) {
+  //     super(props)
+  //     this.state = {selectedOptionId: 'None'}
+  //   }
+
+  optionSelected = event => {
+    // console.log(event.target)
+    const {optionWasClicked} = this.props
+    console.log(event)
+    optionWasClicked(event.target.id)
+  }
+
+  render() {
+    // const {selectedOptionId} = this.state
+    // console.log('present', selectedOptionId)
+    const {eachOption, classNameForAct} = this.props
+    const {id, optionTitle, description} = eachOption
+    return (
+      <div
+        role="button"
+        className={`options ${classNameForAct}`}
+        id={`optionNum${id}`}
+        onClick={this.optionSelected}
+        tabIndex={0}
+      >
+        <div className="titleAndImageHolder">
+          <p className="optionTitle">{optionTitle}</p>
+          <div>
+            <img
+              className="blueCup"
+              src="https://assets.ccbp.in/frontend/react-js/coffee-planner-blue-cup-img.png"
+              alt="blue cup"
+            />
+          </div>
+        </div>
+        <p>{description}</p>
+      </div>
+    )
+  }
+}
+
+export default QuestionOption
